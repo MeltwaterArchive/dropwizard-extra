517f21bfacfcf8ec9bf98b16b74f533e10563929
Nick Telford <nick.telford@gmail.com>
2012-06-19 17:54:47 +0100

    Fixed invalid flushInterval for HBase clients
    Since the change to `Duration`s, we need to convert the `Duration` to
    milliseconds.

--- dropwizard-hbase/src/main/scala/com/datasift/dropwizard/hbase/HBase.scala
+++ dropwizard-hbase/src/main/scala/com/datasift/dropwizard/hbase/HBase.scala
@@ -20,7 +20,7 @@ object HBase {
    */
   def apply(conf: HBaseClientConfiguration, env: Environment): HBase = {
     val client = new HBaseClient(conf.zookeeper.quorumSpec)
-    client.setFlushInterval(conf.flushInterval)
+    client.setFlushInterval(conf.flushInterval.toMilliseconds.toShort)
     client.setIncrementBufferSize(conf.incrementBufferSize)
 
     // determine the appropriate HBase type to use
