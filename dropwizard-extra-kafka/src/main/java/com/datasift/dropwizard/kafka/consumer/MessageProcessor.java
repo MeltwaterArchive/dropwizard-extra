1423eb33cd54f796de6a95579e626237a5a85691
Nick Telford <nick.telford@gmail.com>
2012-08-09 16:18:28 +0100

    Add instrumentation to MessageProcessor
    Now times the processing of each message with a `Timer`.

--- dropwizard-extra-kafka/src/main/java/com/datasift/dropwizard/kafka/consumer/MessageProcessor.java
+++ dropwizard-extra-kafka/src/main/java/com/datasift/dropwizard/kafka/consumer/MessageProcessor.java
@@ -1,11 +1,44 @@
 package com.datasift.dropwizard.kafka.consumer;
 
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Metric;
+import com.yammer.metrics.core.MetricsRegistry;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.core.TimerContext;
+
 /**
  * Processes messages of type {@code T} from a Kafka message stream.
+ *
+ * This {@link StreamProcessor} is instrumented with {@link Metric}s;
+ * specifically, a {@link Timer} that tracks the time taken to process each
+ * message in the stream.
  *
  * @param <T> the decoded type of the message to process
  */
 public abstract class MessageProcessor<T> implements StreamProcessor<T> {
+
+    /**
+     * {@link Timer} for the processing of each message in the stream.
+     */
+    private final Timer processed;
+
+    /**
+     * Creates a MessageProcessor; registers {@link Metric}s with the
+     * {@link Metrics#defaultRegistry() default registry}.
+     */
+    public MessageProcessor() {
+        this(Metrics.defaultRegistry());
+    }
+
+    /**
+     * Creates a MessageProcessor; registers {@link Metric}s with the given
+     * {@link MetricsRegistry}.
+     *
+     * @param registry the {@link MetricsRegistry} to register metrics with
+     */
+    public MessageProcessor(MetricsRegistry registry) {
+        processed = registry.newTimer(getClass(), "processed");
+    }
 
     /**
      * Processes a {@code message} of type {@code T}.
@@ -25,7 +58,9 @@ public abstract class MessageProcessor<T> implements StreamProcessor<T> {
      */
     public void process(final Iterable<T> stream, final String topic) {
         for (final T message : stream) {
+            final TimerContext context = processed.time();
             process(message, topic);
+            context.stop();
         }
     }
 }
