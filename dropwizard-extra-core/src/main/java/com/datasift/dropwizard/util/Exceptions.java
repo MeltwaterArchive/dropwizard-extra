0a4b20a335666d97f5b504d3ead47d82c4138cb4
Nick Telford <nick.telford@gmail.com>
2012-09-14 18:53:20 +0100

    Add misc reflection utilities
    When working with `Class` objects with reflection, there's often a lot
    of boilerplate involved.
    
    Three utility classes provide static methods for working with
    reflection:
    
      * `Classes` - Provides a mechanism for using reflection to instantiate
        a `Class` with a given list of arguments, and resolving
        the applicable `Constructor`.
      * `Primitives` - For more formally inspecting primitive types and
        their boxed counter-parts, as well as auto-casting between the two.
      * `Exceptions` - Provides a mechanism for using reflection to
        instantiate an `Exception` with a given list of arguments. A lot
        like `Classes`.
    
    These are quite advanced and should only be used if you definitely need
    to use reflection and require their functionality. They've been
    documented thoroughly so that they aren't ambiguous as to their
    functionality.

---
+++ dropwizard-extra-core/src/main/java/com/datasift/dropwizard/util/Exceptions.java
@@ -0,0 +1,104 @@
+package com.datasift.dropwizard.util;
+
+/**
+ * Utilities for working with {@link Exception}s.
+ */
+public class Exceptions {
+
+    /**
+     * Creates a new {@link Exception} instance from the given {@link Class}, using the given args.
+     * <p/>
+     * A new {@link Exception} instance object of the given {@link Class} is created, using
+     * reflection, providing the given arguments to the constructor.
+     *
+     * @param clazz the {@link Class} of the {@link Exception} to instantiate.
+     * @param args the arguments to pass to the constructor of the {@link Exception}.
+     * @param <T> the type of the {@link Exception} to return.
+     * @return a new instance of the given {@link Exception}, constructed using the given args.
+     * @throws RuntimeException if there was a problem instantiating the {@link Exception}.
+     */
+    public static <T extends Exception> T newInstance(final Class<T> clazz, final Object... args) {
+        try {
+            return Classes.newInstance(clazz, args);
+        } catch (final Exception e) { // TODO: be less lazy with the Exception handling?
+            throw new RuntimeException("Exception while instantiating" + clazz, e);
+        }
+    }
+
+    /**
+     * Creates a new {@link Exception} instance of the same {@link Class} as the given
+     * <i>template</i>, using the given constructor args.
+     * <p/>
+     * A new {@link Exception} instance object of the given {@link Class} is created, using
+     * reflection, providing the given arguments to the constructor.
+     *
+     * @param template an object that provides the {@link Class} of the {@link Exception } to
+     *                 instantiate.
+     * @param args the arguments to pass to the constructor of the {@link Exception}.
+     * @param <T> the type of the {@link Exception} to return.
+     * @return a new instance of the given {@link Exception}, constructed using the given args.
+     * @throws RuntimeException if there was a problem instantiating the {@link Exception}.
+     */
+    public static <T extends Exception> T newInstanceFrom(final T template, final Object... args) {
+        try {
+            return Classes.newInstanceFrom(template, args);
+        } catch (final Exception e) { // TODO: be less lazy with the Exception handling?
+            throw new RuntimeException("Exception while instantiating" + template.getClass(), e);
+        }
+    }
+
+    /**
+     * Creates a new {@link Exception} instance from the given {@link Class}, using the given args,
+     * ignoring visibility.
+     * <p/>
+     * A new {@link Exception} instance object of the given {@link Class} is created, using
+     * reflection, providing the given arguments to the constructor.
+     * <p/>
+     * The visibility of the constructor defined by the arguments is ignored and a new instance
+     * created irrespective of the defined visibility. This is potentially dangerous, as the API
+     * likely makes no guarantee as to the behaviour when instantiating from a non-public
+     * constructor.
+     *
+     * @param clazz the {@link Class} of the {@link Exception} to instantiate.
+     * @param args the arguments to pass to the constructor of the {@link Exception}.
+     * @param <T> the type of the {@link Exception} to return.
+     * @return a new instance of the given {@link Exception}, constructed using the given args.
+     * @throws RuntimeException if there was a problem instantiating the {@link Exception}.
+     */
+    public static <T extends Exception> T unsafeNewInstance(final Class<T> clazz,
+                                                            final Object... args) {
+        try {
+            return Classes.unsafeNewInstance(clazz, args);
+        } catch (final Exception e) { // TODO: be less lazy with the Exception handling?
+            throw new RuntimeException("Exception while instantiating" + clazz, e);
+        }
+    }
+
+    /**
+     * Creates a new {@link Exception} instance of the same {@link Class} as the given
+     * <i>template</i>, using the given constructor args, ignoring visibility.
+     * <p/>
+     * A new {@link Exception} instance object of the given {@link Class} is created, using
+     * reflection, providing the given arguments to the constructor.
+     * <p/>
+     * The visibility of the constructor defined by the arguments is ignored and a new instance
+     * created irrespective of the defined visibility. This is potentially dangerous, as the API
+     * likely makes no guarantee as to the behaviour when instantiating from a non-public
+     * constructor.
+     *
+     * @param template an object that provides the {@link Class} of the {@link Exception } to
+     *                 instantiate.
+     * @param args the arguments to pass to the constructor of the {@link Exception}.
+     * @param <T> the type of the {@link Exception} to return.
+     * @return a new instance of the given {@link Exception}, constructed using the given args.
+     * @throws RuntimeException if there was a problem instantiating the {@link Exception}.
+     */
+    public static <T extends Exception> T unsafeNewInstanceFrom(final T template,
+                                                                final Object... args) {
+        try {
+            return Classes.unsafeNewInstanceFrom(template, args);
+        } catch (final Exception e) { // TODO: be less lazy with the Exception handling?
+            throw new RuntimeException("Exception while instantiating" + template.getClass(), e);
+        }
+    }
+}
