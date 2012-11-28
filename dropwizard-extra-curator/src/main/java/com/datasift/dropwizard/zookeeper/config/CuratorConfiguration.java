a70fd1b9741e592e7c1e2ea66163eb7581043bc3
Nick Telford <nick.telford@gmail.com>
2012-11-28 15:33:51 +0000

    Extract `RetryPolicy` from `CuratorConfiguration`

--- dropwizard-extra-curator/src/main/java/com/datasift/dropwizard/zookeeper/config/CuratorConfiguration.java
+++ dropwizard-extra-curator/src/main/java/com/datasift/dropwizard/zookeeper/config/CuratorConfiguration.java
@@ -1,24 +1,32 @@
 package com.datasift.dropwizard.zookeeper.config;
 
-import com.netflix.curator.retry.ExponentialBackoffRetry;
-import com.netflix.curator.retry.RetryNTimes;
-import com.netflix.curator.retry.RetryUntilElapsed;
 import com.yammer.dropwizard.util.Duration;
-import org.codehaus.jackson.annotate.JsonCreator;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
- * TODO: Document
+ * A configuration for a {@link com.netflix.curator.framework.CuratorFramework} instance.
  */
 public class CuratorConfiguration {
 
-    private static final Duration DEFAULT_SLEEP_DURATION = Duration.seconds(3);
-
+    /**
+     * The configuration of the ZooKeeper ensemble to use.
+     */
     protected ZooKeeperConfiguration ensemble = new ZooKeeperConfiguration();
 
-    protected RetryPolicy retryPolicy = new RetryPolicy("retry 5 times sleeping for 3 seconds");
+    /**
+     * The policy to take in the event of failures.
+     * <p/>
+     * The following options are availble:
+     * <ul>
+     *     <li><i>Exponential Backoff</i> -
+     *     "backoff [upto] &lt;N&gt; times [sleeping [for] M (seconds|millis|...)"</li>
+     *     <li><i>Fixed Retries</i> - "[retry] N times [sleeping [for] M (seconds|millis|...)"</li>
+     *     <li><i>Timed Retries</i> -
+     *     "[retry] until N (seconds|milli|...) [sleeping [for] M (seconds|millis|...)"</li>
+     * </ul>
+     *
+     * @see RetryPolicy#parse(String)
+     */
+    protected RetryPolicy retryPolicy = RetryPolicy.times(5, Duration.seconds(3));
 
     public ZooKeeperConfiguration getEnsembleConfiguration() {
         return ensemble;
@@ -26,54 +34,5 @@ public class CuratorConfiguration {
 
     public com.netflix.curator.RetryPolicy getRetryPolicy() {
         return retryPolicy.getPolicy();
-    }
-
-    class RetryPolicy {
-
-        private final Pattern backoffPattern = Pattern.compile(
-                "(?i)backoff\\s+(?:upto\\s+)?(\\d+)\\s+times(?:\\s+sleeping(?:\\s+for)?\\s+(\\d+\\s*\\w+))?");
-        private final Pattern fixedRetryPattern = Pattern.compile(
-                "(?i)(?:retry\\s+)?(\\d+)\\s+times(?:\\s+sleeping(?:\\s+for)?\\s+(\\d+\\s*\\w+))?");
-        private final Pattern boundedRetryPattern = Pattern.compile(
-                "(?i)(?:retry\\s+)?until\\s+(\\d+\\s*\\w+)(?:\\s+sleeping(?:\\s+for)?\\s+(\\d+\\s*\\w+))?");
-
-        private final com.netflix.curator.RetryPolicy policy;
-
-        @JsonCreator
-        public RetryPolicy(final String value) {
-            final Matcher backoffMatcher = backoffPattern.matcher(value);
-            final Matcher fixedMatcher = fixedRetryPattern.matcher(value);
-            final Matcher boundedMatcher = boundedRetryPattern.matcher(value);
-
-            if (backoffMatcher.matches()) {
-                final String sleep = backoffMatcher.group(2);
-                final Duration sleepTime = sleep.isEmpty()
-                        ? DEFAULT_SLEEP_DURATION
-                        : Duration.parse(sleep);
-
-                policy = new ExponentialBackoffRetry(
-                        (int) sleepTime.toMilliseconds(), Integer.parseInt(backoffMatcher.group(1)));
-            } else if (fixedMatcher.matches()) {
-                final String sleep = fixedMatcher.group(2);
-                final Duration sleepTime = sleep.isEmpty()
-                        ? DEFAULT_SLEEP_DURATION
-                        : Duration.parse(sleep);
-                policy = new RetryNTimes(
-                        Integer.parseInt(fixedMatcher.group(1)), (int) sleepTime.toMilliseconds());
-            } else if (boundedMatcher.matches()) {
-                final String sleep = boundedMatcher.group(2);
-                final Duration sleepTime = sleep.isEmpty()
-                        ? DEFAULT_SLEEP_DURATION
-                        : Duration.parse(sleep);
-                policy = new RetryUntilElapsed(
-                        Integer.parseInt(boundedMatcher.group(1)), (int) sleepTime.toMilliseconds());
-            }
-
-            throw new IllegalArgumentException("Invalid RetryPolicy: " + value);
-        }
-
-        public com.netflix.curator.RetryPolicy getPolicy() {
-            return policy;
-        }
     }
 }
