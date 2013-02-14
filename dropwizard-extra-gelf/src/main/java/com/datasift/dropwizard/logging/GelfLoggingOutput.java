a20ee088f6e6df88b13af299e54b812e7a7e56ce
Nick Telford <nick.telford@gmail.com>
2013-02-14 11:26:30 +0000

    Improve GELF Appender
    Adds maximum chunksize setting, names the Appender and gives it the
    correct LoggerContext.

--- dropwizard-extra-gelf/src/main/java/com/datasift/dropwizard/logging/GelfLoggingOutput.java
+++ dropwizard-extra-gelf/src/main/java/com/datasift/dropwizard/logging/GelfLoggingOutput.java
@@ -8,16 +8,19 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.yammer.dropwizard.logging.LoggingOutput;
+import com.yammer.dropwizard.util.Size;
 import me.moocar.logbackgelf.GelfAppender;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.Map;
 
 /**
  * A {@link LoggingOutput} for writing log messages to a graylog2 server.
+ *
+ * TODO: write our own Appender to avoid the dependency on Gson.
  */
 @JsonTypeName("gelf")
 public class GelfLoggingOutput implements LoggingOutput {
@@ -45,35 +48,82 @@ public class GelfLoggingOutput implements LoggingOutput {
     @Pattern(regexp = "[0-9]+\\.[0-9]+(\\.[0-9]+)?")
     private String graylogVersion = "0.9.5";
 
+    /**
+     * The maximum size of each GELF chunk to send to the server.
+     */
     @JsonProperty
-    @Min(1000)
-    private int chunkThreshold = 1000;
+    @NotNull
+    private Size chunkSize = Size.kilobytes(1);
 
+    /**
+     * The format of the log message as a Logback log pattern.
+     */
     @JsonProperty
     private String logFormat;
 
+    /**
+     * Additional fields for the log message as a mapping of the SLF4J MDC name
+     * to the GELF field.
+     *
+     * <b>Example, adding an IP address for requests</b>
+     * In application code:
+     * <code>
+     *     org.slf4j.MDC.put("ipAddress", getClientIpAddress());
+     * </code>
+     * In configuration (YAML):
+     * <code>
+     *     logging:
+     *         outputs:
+     *             - type: "gelf"
+     *             ...
+     *             - additionalFields:
+     *                 ipAddress: _ip_address
+     * </code>
+     * By convention, GELF fields <i>should</i> begin with an underscore.
+     */
     @JsonProperty
     private Map<String, String> additionalFields = ImmutableMap.of();
 
+    /**
+     * Builds an {@link Appender} for writing log messages to a graylog2 server.
+     *
+     * @param context the context of the Logger to build the Appender for.
+     * @param serviceName the name of the service to build the Appender for.
+     *
+     * @return an Appender for writing log messages to a graylog2 server.
+     */
     @Override
     public Appender<ILoggingEvent> build(final LoggerContext context,
                                          final String serviceName) {
 
         final GelfAppender<ILoggingEvent> appender = new GelfAppender<ILoggingEvent>();
+
+        // use service name for GELF facility
         appender.setFacility(serviceName);
+
+        // configure GELF Appender
         appender.setGraylog2ServerHost(host);
         appender.setGraylog2ServerPort(port);
+        appender.setChunkThreshold((int) chunkSize.toBytes());
         appender.setUseLoggerName(true);
         appender.setUseThreadName(true);
         appender.setAdditionalFields(additionalFields);
 
+        // configure log format only when defined
         if (!Strings.isNullOrEmpty(logFormat)) {
             appender.setMessagePattern(logFormat);
         }
 
+        // configure protocol version only when defined
         if (!Strings.isNullOrEmpty(graylogVersion)) {
             appender.setGraylog2ServerVersion(graylogVersion);
         }
+
+        // configure name and context
+        appender.setName("GELF");
+        appender.setContext(context);
+
+        appender.start();
 
         return appender;
     }
