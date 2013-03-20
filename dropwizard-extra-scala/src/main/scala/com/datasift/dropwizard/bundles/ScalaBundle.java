76d3c1e27bad82a0ef9425214a044e4ae3c70299
Nick Telford <nick.telford@gmail.com>
2013-03-20 23:19:04 +0000

    Begin import of upstream Scala support
    Provides a `ScalaService` base and a (currently empty) `ScalaBundle` for
    all core Scala functionality.

---
+++ dropwizard-extra-scala/src/main/scala/com/datasift/dropwizard/bundles/ScalaBundle.java
@@ -0,0 +1,20 @@
+package com.datasift.dropwizard.bundles;
+
+import com.yammer.dropwizard.Bundle;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+
+/**
+ * Bundle that provides Scala support to core functionality of Dropwizard Services.
+ */
+public class ScalaBundle implements Bundle {
+
+    @Override
+    public void initialize(final Bootstrap<?> bootstrap) {
+    }
+
+    @Override
+    public void run(final Environment environment) {
+
+    }
+}
