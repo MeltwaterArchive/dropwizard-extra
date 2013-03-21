b192f51c31d07abedcf8e868a729836d4b8c9c01
Nick Telford <nick.telford@gmail.com>
2013-03-21 02:34:24 +0000

    Replaced GELF Appender with custom Appender
    No longer depends on a third-party Appender, which cuts out the
    transitive dependency on Gson. Instead uses a Jackson `ObjectMapper` to
    do the JSON work.

---
+++ dropwizard-extra-gelf/src/main/java/com/datasift/dropwizard/logging/gelf/GelfAppender.java
@@ -0,0 +1,158 @@
+package com.datasift.dropwizard.logging.gelf;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.status.ErrorStatus;
+import ch.qos.logback.core.status.WarnStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Charsets;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * TODO (21/03/13): Document
+ * TODO: verify lifecycle makes sense/won't cause unexpected problems
+ */
+public class GelfAppender<E extends ILoggingEvent> extends AppenderBase<E> {
+
+    private MessageDigest MD5;
+    private GelfLayout layout;
+    private SocketAddress target;
+    private DatagramSocket socket;
+    private String hostname = "localhost";
+    private int port = 12201;
+    private int chunkSize = 1024;
+
+    public GelfAppender<E> setLayout(final GelfLayout layout) {
+        this.layout = layout;
+        return this;
+    }
+
+    public GelfAppender<E> setHostname(final String hostname) {
+        this.hostname = hostname;
+        return this;
+    }
+
+    public GelfAppender<E> setPort(final int port) {
+        this.port = port;
+        return this;
+    }
+
+    public GelfAppender<E> setChunkSize(final int chunkSize) {
+        this.chunkSize = chunkSize;
+        return this;
+    }
+
+    @Override
+    public void start() {
+        try {
+            MD5 = MessageDigest.getInstance("MD5");
+        } catch (final NoSuchAlgorithmException e) {
+            error("Unable to load MD5 implementation", e);
+            return;
+        }
+
+        try {
+            socket = new DatagramSocket();
+        } catch (final SocketException e) {
+            error("Failed to bind local UDP socket", e);
+            return;
+        }
+
+        target = new InetSocketAddress(hostname, port);
+
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        if (socket != null && !socket.isClosed()) {
+            socket.close();
+        }
+        socket = null;
+
+        super.stop();
+    }
+
+    @Override
+    protected void append(final E event) {
+
+        if (!isStarted()) {
+            warn("Appender not started yet");
+            return;
+        }
+
+        if (socket == null) {
+            // todo: we already mentioned that the socket can't be bound, right?
+            return;
+        }
+
+        // format event using GelfLayout
+        final String data = layout.doLayout(event);
+
+        try {
+            // compress event with zlib/gzip
+            final byte[] compressed = compress(data);
+
+            // generate message header
+            // todo: test/verify this algorithm
+            final byte total = (byte) Math.ceil(compressed.length / (chunkSize - 12));
+            final byte[] header = new byte[] { 0x1e, 0x0f, 0, 0, 0, 0, 0, 0, 0, 0, 1, total };
+            final byte[] id = Arrays.copyOf(MD5.digest(compressed), 8);
+            System.arraycopy(id, 0, header, 2, 8);
+            final byte[][] chunks = new byte[total][];
+
+            // generate message chunks
+            // todo: test/verify this algorithm
+            for (byte i = 0; i < total; i++) {
+                chunks[i] = new byte[chunkSize];
+                System.arraycopy(header, 0, chunks[i], 0, 12);
+                chunks[i][10] = i;
+                System.arraycopy(compressed, i * (chunkSize - 12), chunks[i], 12, Math.min((chunkSize - 12), compressed.length - (i * (chunkSize - 12))));
+            }
+
+            // send chunks over the wire
+            for (final byte[] chunk : chunks) {
+                socket.send(new DatagramPacket(chunk, chunk.length, target));
+            }
+
+        } catch (final IOException e) {
+            getContext().getStatusManager().add(new ErrorStatus(e.getMessage(), this, e));
+        }
+    }
+
+    private byte[] compress(final String data) throws IOException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            final GZIPOutputStream compressed = new GZIPOutputStream(out);
+            try {
+                compressed.write(data.getBytes(Charsets.UTF_8));
+                compressed.close();
+                out.close();
+                return out.toByteArray();
+            } finally {
+                compressed.close();
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    private void warn(final String message) {
+        getContext().getStatusManager().add(new WarnStatus(message, this));
+    }
+
+    private void error(final String message) {
+        error(message, null);
+    }
+
+    private void error(final String message, final Throwable t) {
+        getContext().getStatusManager().add(new ErrorStatus(message, this, t));
+    }
+}
--- dropwizard-extra-gelf/src/main/java/com/datasift/dropwizard/logging/gelf/GelfLoggingOutput.java
+++ dropwizard-extra-gelf/src/main/java/com/datasift/dropwizard/logging/gelf/GelfLoggingOutput.java
@@ -1,21 +1,29 @@
-package com.datasift.dropwizard.logging;
+package com.datasift.dropwizard.logging.gelf;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.yammer.dropwizard.logging.LoggingOutput;
 import com.yammer.dropwizard.util.Size;
-import me.moocar.logbackgelf.GelfAppender;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * A {@link LoggingOutput} for writing log messages to a graylog2 server.
@@ -40,15 +48,6 @@ public class GelfLoggingOutput implements LoggingOutput {
     private int port;
 
     /**
-     * The version of the Graylog2 server.
-     *
-     * Defaults to 0.9.5.
-     */
-    @JsonProperty
-    @Pattern(regexp = "[0-9]+\\.[0-9]+(\\.[0-9]+)?")
-    private String graylogVersion = "0.9.5";
-
-    /**
      * The maximum size of each GELF chunk to send to the server.
      */
     @JsonProperty
@@ -62,8 +61,7 @@ public class GelfLoggingOutput implements LoggingOutput {
     private String logFormat;
 
     /**
-     * Additional fields for the log message as a mapping of the SLF4J MDC name
-     * to the GELF field.
+     * SLF4J MDC fields to include in the messages sent via GELF.
      *
      * <b>Example, adding an IP address for requests</b>
      * In application code:
@@ -76,13 +74,11 @@ public class GelfLoggingOutput implements LoggingOutput {
      *         outputs:
      *             - type: "gelf"
      *             ...
-     *             - additionalFields:
-     *                 ipAddress: _ip_address
+     *             - includes: [ "ipAddress" ]
      * </code>
-     * By convention, GELF fields <i>should</i> begin with an underscore.
      */
     @JsonProperty
-    private Map<String, String> additionalFields = ImmutableMap.of();
+    private String[] includes = new String[0];
 
     /**
      * Builds an {@link Appender} for writing log messages to a graylog2 server.
@@ -96,35 +92,56 @@ public class GelfLoggingOutput implements LoggingOutput {
     public Appender<ILoggingEvent> build(final LoggerContext context,
                                          final String serviceName) {
 
-        final GelfAppender<ILoggingEvent> appender = new GelfAppender<ILoggingEvent>();
+        final GelfLayout layout = new GelfLayout(new ObjectMapper()); // todo: find a way to provide this from the Environment?
 
         // use service name for GELF facility
-        appender.setFacility(serviceName);
-
-        // configure GELF Appender
-        appender.setGraylog2ServerHost(host);
-        appender.setGraylog2ServerPort(port);
-        appender.setChunkThreshold((int) chunkSize.toBytes());
-        appender.setUseLoggerName(true);
-        appender.setUseThreadName(true);
-        appender.setAdditionalFields(additionalFields);
+        layout.setFacility(serviceName);
+        layout.setHostname(getLocalHost());
+        layout.setContext(context);
+        layout.setAdditionalFields(includes);
 
         // configure log format only when defined
         if (!Strings.isNullOrEmpty(logFormat)) {
-            appender.setMessagePattern(logFormat);
+            layout.setPattern(logFormat);
         }
 
-        // configure protocol version only when defined
-        if (!Strings.isNullOrEmpty(graylogVersion)) {
-            appender.setGraylog2ServerVersion(graylogVersion);
-        }
+        final GelfAppender<ILoggingEvent> appender = new GelfAppender<ILoggingEvent>();
 
-        // configure name and context
-        appender.setName("GELF");
+        appender.setLayout(layout);
         appender.setContext(context);
+        appender.setHostname(host);
+        appender.setPort(port);
+        appender.setChunkSize((int) chunkSize.toBytes());
 
         appender.start();
 
         return appender;
+    }
+
+    private String getLocalHost() {
+        try {
+            final Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface iface = null;
+            while (iface == null && ifaces != null && ifaces.hasMoreElements()) {
+                iface = ifaces.nextElement();
+            }
+
+            if (iface == null) {
+                // cannot find a network interface
+                return "0.0.0.0";
+            }
+
+            final Enumeration<InetAddress> addresses = iface.getInetAddresses();
+            InetAddress address = null;
+            while (address == null && addresses.hasMoreElements()) {
+                address = addresses.nextElement();
+            }
+
+            return address == null
+                    ? "0.0.0.0" // no network interface has a valid network address
+                    : address.getHostAddress();
+        } catch (final SocketException se) {
+            return "0.0.0.0"; // cannot inspect network interfaces
+        }
     }
 }
