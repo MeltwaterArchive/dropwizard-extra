afdfb8a94f4fafd2077b216794ee9881b7a38e9b
Nick Telford <nick.telford@gmail.com>
2013-04-02 12:42:53 +0100

    Improve consistency of Scala jDBI implicit wrapper

--- dropwizard-extra-scala/src/main/scala/com/datasift/dropwizard/jdbi/DBI.scala
+++ dropwizard-extra-scala/src/main/scala/com/datasift/dropwizard/jdbi/DBI.scala
@@ -20,3 +20,21 @@ object DBI {
     new DBIFactory().build(env, conf, conf.getUrl)
   }
 }
+
+/**
+ * Provides enhancements to the Dropwizard DB API.
+ *
+ * @param db [[org.skife.jdbi.v2.DBI]] to wrap
+ */
+class DBI(db: org.skife.jdbi.v2.DBI) {
+
+  /**
+   * Creates a DAO instance.
+   *
+   * @tparam T type of the DAO to create
+   * @return a DAO instance for the specified type
+   */
+  def daoFor[T : Manifest]: T = {
+    db.onDemand[T](manifest[T].erasure.asInstanceOf[Class[T]])
+  }
+}
